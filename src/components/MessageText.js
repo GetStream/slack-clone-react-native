9aa546844acc2eead86575e1559213e635f343bf
vishtree <vishalnarkhede.iitd@gmail.com>
2020-09-25 15:35:31 +0200

    Updating stream-chat-react-native@1.3.0

--- src/components/MessageText.js
+++ src/components/MessageText.js
@@ -5,7 +5,10 @@ export const MessageText = props => {
   return (
     <React.Fragment>
       {props.message.attachments.length === 0 && <MessageUserBar {...props} />}
-      {props.renderText(props.message, props.theme.message.content.markdown)}
+      {props.renderText({
+        message: props.message,
+        markdownRules: props.theme.message.content.markdown,
+      })}
     </React.Fragment>
   );
 };
